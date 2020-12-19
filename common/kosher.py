9c6b28344a5ec9643a669159b9bf6aabfab95258
Forest Mars <themarsgroup@gmail.com>
2020-12-19 05:59:13 -0500

    aes, apparently'

--- common/kosher.py
+++ common/kosher.py
@@ -10,7 +10,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-#from Crypto.Cipher import AES
+from Crypto.Cipher import AES
 #from Crypto.Random import get_random_bytes
 #from Crypto.Hash import HMAC, SHA, SHA256, SHA384, SHA512
 
@@ -61,12 +61,25 @@ class KosherPickleMixin(object):
         return base64.urlsafe_b64encode(kdf.derive(pw))
 
 
+    def aes_iv():
+        iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+        #return 'foo'
+
+    def aes():
+        aes = AES.new(self.derive_key, AES.MODE_CBC, self.aes.iv)
+        return aes
+
+
+
     def scheme(self, key): # aka scheme
         """ N.B. Currently supports only Fernet (symmetric) encryption. """
         #if self._algo  in globals() and isinstance(globals()[self._algo], types.ClassType):
         if self._algo in globals():
+            input('ha')
+            input(str(self._algo))
 
-            return globals()[self._algo](key)
+            input(self._algos[self._algo])
+            return globals()[self._algo](key, self._algo[0])
 
 
     def encrypt(self, raw_data, pw, protocol=None, fix_imports=True):
@@ -101,7 +114,7 @@ class KosherPickleMixin(object):
     @property
     def _algo(self):
         """ Currently Kosher only uses Fernet. """
-        self.algo = 'Fernet'
+        self.algo = 'AES'
         return self.algo
 
 
@@ -112,8 +125,10 @@ class KosherPickleMixin(object):
         else:
             print("Sorry, " + algor + " is not currently supported.")
 
-
-    algos = [
-        'AES',
-        'Fernet',
-        ]
+    @property
+    def algos():
+        self.algos = [
+            {'AES': [AES.MODE_CBC, self.aes.iv]},
+            {'Fernet': ['']},
+            ]
+        return self.algos
