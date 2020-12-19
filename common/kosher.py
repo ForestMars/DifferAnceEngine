5138132f694ae6d422e0f7f73f22d3d48541a45c
Forest Mars <themarsgroup@gmail.com>
2020-12-19 05:43:58 -0500

    scheme

--- common/kosher.py
+++ common/kosher.py
@@ -3,6 +3,7 @@
 import os
 import base64
 import pickle
+from typing import types
 
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
@@ -36,6 +37,7 @@ class KosherPickleMixin(object):
         else:
             return self.__dict__
 
+
     def __setstate__(self, obj):
         if isinstance(obj, bytes) and 'MOT' in globals():
             try:
@@ -44,6 +46,7 @@ class KosherPickleMixin(object):
                 print(e)
         else:
             return obj.__dict__
+
 
     def derive_key(self, pw, salt, sha=SHA):
         pw = pw.encode()
@@ -57,25 +60,31 @@ class KosherPickleMixin(object):
 
         return base64.urlsafe_b64encode(kdf.derive(pw))
 
-    def encryption(self, key):
-        """ N.B. Currently supports only Fernet (symmetric) encryption. """
 
-        return self._algo
+    def encryption(self, key): # aka scheme
+        """ N.B. Currently supports only Fernet (symmetric) encryption. """
+        #if self._algo  in globals() and isinstance(globals()[self._algo], types.ClassType):
+        if self._algo in globals():
+
+            return globals()[self._algo](key)
+
 
     def encrypt(self, raw_data, pw, protocol=None, fix_imports=True):
         salt = os.urandom(SALT_SIZE)
         key = self.derive_key(pw, salt)
         #encryption = self.encryption(key)
-        fernet = Fernet(key)
+        #fernet = Fernet(key)
+        scheme = self.encryption(key)
         pickled_data = pickle.dumps(
             raw_data,
             protocol=protocol,
             fix_imports=fix_imports
             )
 
-        encrypted_data = fernet.encrypt(pickled_data)
+        encrypted_data = scheme.encrypt(pickled_data)
 
         return salt + encrypted_data
+
 
     def decrypt(self, input_data, pw, fix_imports=True, encoding="ASCII", errors="strict"):
         salt = input_data[:SALT_SIZE]
@@ -91,19 +100,22 @@ class KosherPickleMixin(object):
             errors=errors
             )
 
-    @property
-    def algo(self):
-        """ Currently Kosher only uses Fernet. """
-        self._algo = 'fernet'
-        return self._algo
 
-    @algo.setter
-    def algo(self, algo: str):
-        if algo in self.algos:
-            self._algo = algo
+    @property
+    def _algo(self):
+        """ Currently Kosher only uses Fernet. """
+        self.algo = 'Fernet'
+        return self.algo
+
+
+    @_algo.setter
+    def _algo(self, algor: str):
+        if algor in self.algos:
+            self.algo = algor
         else:
-            print("Sorry, " + algo + " is not currently supported.")
+            print("Sorry, " + algor + " is not currently supported.")
+
 
     algos = [
-        'fernet',
+        'Fernet',
         ]
